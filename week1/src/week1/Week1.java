f4ccb67031f6c057cde524e79bc3fe5eb1240e8e
mug31 <mugteadotco@gmail.com>
2025-02-28 11:28:47 +0700

    Week1

---
+++ week1/src/week1/Week1.java
@@ -0,0 +1,12 @@
+package week1;
+public class Week1 {
+
+   int[] data = {5,10,15,20,30};
+   
+    public static void main(String[] args) {
+         Week1 obj = new Week1();
+        for(int i=0; i<obj.data.length -1 ; i++){
+            System.out.print(obj.data[i]+" ");
+        }
+    }
+}
