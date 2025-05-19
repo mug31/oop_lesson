fad2605d7274a188781e90951df23f629f2b8906
mug31 <mugteadotco@gmail.com>
2025-05-19 10:02:29 +0700

    kalo ini keknya inherintance deh

---
+++ b4_uts/src/b4_uts/tes2.java
@@ -0,0 +1,19 @@
+package b4_uts;
+class Hitung {
+
+    static int Perkalian(int a, int b) {
+        return a * b;
+    }
+    
+    static double Perkalian(double a, double b) {
+        return a * b;
+    }
+}
+
+class polymorfOverload {
+
+    public static void main(String[] args) {
+        System.out.println(Hitung.Perkalian(2, 4));
+        System.out.println(Hitung.Perkalian(5.5, 6.3));
+    }
+}
